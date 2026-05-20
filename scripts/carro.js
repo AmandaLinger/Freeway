98d5751501f6c9930fe79dc8e8491f77ea134a3d
Amanda Marques <amandamarqueslinger@gmail.com>
2026-05-20 15:13:31 -0300

    melhorando legibilidade do código

--- scripts/carro.js
+++ scripts/carro.js
@@ -1,11 +1,27 @@
-let xCarro = 450;
-let yCarro = 40;
+
+let xCarros = [500,500,500];
+let yCarros =[40,96,150,];
+let velocidadeCarros = [2,2.5,3];
 
 function mostraCarro(){
-    image(imagemCarro,xCarro,yCarro,50,40)
+    for(let i = 0; i < imagemCarros.length; i++){
+        image(imagemCarros[i],xCarros[i],yCarros[i],50,40)
+    }
 }
 
 function movimentaCarro(){
-    xCarro -= 2
+    for(let i = 0; i < imagemCarros.length; i++){
+        xCarros[i] -= velocidadeCarros[i]
+    }
 }
 
+function voltaPosicaoInicialDoCarro(){
+    for(let i = 0; i < imagemCarros.length; i++)
+    if(passouTodaTela(xCarros[i])){
+        xCarros[i] = 500
+    }
+}
+
+function passouTodaTela(xCarro){
+    return xCarro<-50
+}
--- scripts/imagens.js
+++ scripts/imagens.js
@@ -1,9 +1,14 @@
 let imagemEstrada;
 let imagemDoAtor;
 let imagemCarro;
+let imagemCarro2;
+let imagemCarro3;
 
 function preload(){
   imagemEstrada = loadImage("img/estrada.png");
   imagemDoAtor = loadImage("img/ator-1.png");
   imagemCarro = loadImage("img/carro-1.png");
+  imagemCarro2 = loadImage("img/carro-2.png");
+  imagemCarro3 = loadImage("img/carro-3.png");
+  imagemCarros = [imagemCarro,imagemCarro2,imagemCarro3]
 }
